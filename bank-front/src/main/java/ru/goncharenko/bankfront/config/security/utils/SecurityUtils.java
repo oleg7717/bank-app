c6f159feb544fd962ca1d9d1b83f4e372a49519b
ogoncharenko <ogoncharenko@interprocom.ru>
2026-04-11 15:53:30 +0300

    feat: Добавлен Gateway

---
+++ bank-front/src/main/java/ru/goncharenko/bankfront/config/security/utils/SecurityUtils.java
@@ -0,0 +1,37 @@
+package ru.goncharenko.bankfront.config.security.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class SecurityUtils {
+
+	public String getCurrentUsername() {
+		try {
+			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+			if (authentication == null) {
+				log.warn("No authentication found in context");
+				return "anonymous";
+			}
+
+			Object principal = authentication.getPrincipal();
+			String username;
+
+			if (principal instanceof DefaultOidcUser user) {
+				username = (String) user.getClaims().get("preferred_username");
+			} else {
+				username = principal != null ? principal.toString() : "unknown";
+			}
+
+			log.info("Current user: {}", username);
+			return username;
+		} catch (Exception e) {
+			log.error("Error getting security context", e);
+			return "anonymous";
+		}
+	}
+}
