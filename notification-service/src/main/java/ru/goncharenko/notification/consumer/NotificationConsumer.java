d2cc0954bb8fcc8dc773026fcaa6f4fc75fd9ded
ogoncharenko <ogoncharenko@interprocom.ru>
2026-05-14 19:24:31 +0300

    config: Настройки consumer factory перенесены в application.yaml

--- notification-service/src/main/java/ru/goncharenko/notification/consumer/NotificationConsumer.java
+++ notification-service/src/main/java/ru/goncharenko/notification/consumer/NotificationConsumer.java
@@ -8,16 +8,19 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 import ru.goncharenko.bankclient.common.model.NotificationDto;
 import ru.goncharenko.notification.service.jpa.NotificationJPA;
+import tools.jackson.databind.ObjectMapper;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class NotificationConsumer {
+	private final ObjectMapper objectMapper;
 	private final NotificationJPA notificationJPA;
 
 	@KafkaListener(topics = "notifications", groupId = "notification-consumer")
-	public void processMessage(ConsumerRecord<String, NotificationDto> record) {
-		notificationJPA.saveNotificationMessage(Mono.just(record.value()))
+	public void processMessage(ConsumerRecord<String, byte[]> record) {
+		var dto = objectMapper.readValue(record.value(), NotificationDto.class);
+		notificationJPA.saveNotificationMessage(Mono.just(dto))
 				.subscribe(
 						null,
 						error -> log.error("Error while saving notification", error)
