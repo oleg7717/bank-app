b09dc788716ec3ae086556e7917ba5e8df9317cd
ogoncharenko <ogoncharenko@interprocom.ru>
2026-05-16 11:46:39 +0300

    test: Добавлен тест producer / consumer kafka

---
+++ cash-service/src/test/java/ru/goncharenko/cash/NotificationKafkaProcessorTest.java
@@ -0,0 +1,66 @@
+package ru.goncharenko.cash;
+
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+import ru.goncharenko.bankclient.common.model.NotificationDto;
+import ru.goncharenko.cash.config.TestKafkaConfig;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.goncharenko.cash.NotificationKafkaProcessorTest.TEST_TOPIC_NAME;
+
+@SpringBootTest
+@EmbeddedKafka(topics = {TEST_TOPIC_NAME})
+@Import({TestKafkaConfig.class})
+public class NotificationKafkaProcessorTest {
+	@Autowired
+	private KafkaTemplate<String, Object> kafkaTemplate;
+
+	@Autowired
+	private EmbeddedKafkaBroker embeddedKafkaBroker;
+
+	final static String TEST_TOPIC_NAME = "notification";
+	private final String service = "cash-service";
+	private final String message = "Пополнение / снятие средств со счёта %s успешно выполнено";
+
+	@Test
+	public void testNotificationProcessor() {
+		try (var consumer = new DefaultKafkaConsumerFactory<String, NotificationDto>(
+				KafkaTestUtils.consumerProps("notification-consumer", "true", embeddedKafkaBroker),
+				new StringDeserializer(),
+				new JsonDeserializer<>(NotificationDto.class)
+		).createConsumer()) {
+			consumer.subscribe(List.of(TEST_TOPIC_NAME));
+			consumer.poll(Duration.ofMillis(100));
+
+			NotificationDto notification = NotificationDto.builder()
+					.service(service)
+					.message(message)
+					.created(LocalDateTime.now())
+					.build();
+			kafkaTemplate.send(TEST_TOPIC_NAME, service, notification).get();
+
+
+			var record = KafkaTestUtils.getSingleRecord(consumer, TEST_TOPIC_NAME, Duration.ofSeconds(5));
+			assertEquals(service, record.key());
+			assertEquals(notification, record.value());
+		} catch (ExecutionException e) {
+			throw new RuntimeException(e);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
